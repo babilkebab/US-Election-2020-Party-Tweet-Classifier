9daa608ade1ce503e1f90b4deb34a9eb4fce60e5
babilkebab <101179902+babilkebab@users.noreply.github.com>
2024-07-31 13:20:50 +0200

    Update main_classifier.py

--- main_classifier.py
+++ main_classifier.py
@@ -2,8 +2,9 @@ import pandas as pd
 from src.TweetClassifier import TweetClassifier
 
 
-final_ds = pd.read_csv("ds/complete_ds.csv", parse_dates=True)
-final_ds["label"] = final_ds["label"].replace({"GOP": 0, "DEM": 1})
-
-classifier = TweetClassifier(final_ds)
-classifier.save_model()
+if __name__ == "__main__":
+    final_ds = pd.read_csv("ds/complete_ds.csv", parse_dates=True)
+    final_ds["label"] = final_ds["label"].replace({"GOP": 0, "DEM": 1})
+    
+    classifier = TweetClassifier(final_ds)
+    classifier.save_model()
